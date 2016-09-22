9718b15340fe36debb73f75e23dcf6147ce7d553
Jakub Kozłowski <kubukoz@gmail.com>
2016-09-22 03:24:53 +0200

    Implement stubs of GithubService's methods

--- src/main/scala/com/kubukoz/trellocat/service/GithubService.scala
+++ src/main/scala/com/kubukoz/trellocat/service/GithubService.scala
@@ -2,7 +2,7 @@ package com.kubukoz.trellocat.service
 
 import akka.stream.Materializer
 import com.kubukoz.trellocat.api.ApiClient
-import com.kubukoz.trellocat.domain.Github.Column
+import com.kubukoz.trellocat.domain.Github.{Column, Project}
 import com.kubukoz.trellocat.domain.{Github, JsonSupport}
 
 import scala.concurrent.Future
@@ -11,18 +11,24 @@ import scala.concurrent.Future
   * Accesses the GitHub API.
   **/
 trait GithubService {
-  def createColumn(projectId: String, column: Column): Future[Unit] = ???
+  def createColumn(projectId: String, column: Column): Future[Unit]
 
-  def createProject(repoId: String, projectName: String): Future[Github.Project] = ???
+  def createProject(repoId: String, projectName: String): Future[Github.Project]
 }
 
 /**
   * Implements [[GithubService]] with a HTTP client.
-  * */
-class RealGithubService(implicit api: ApiClient, mat: Materializer) extends GithubService with JsonSupport{
+  **/
+class RealGithubService(implicit api: ApiClient, mat: Materializer) extends GithubService with JsonSupport {
+  override def createColumn(projectId: String, column: Column): Future[Unit] = ???
 
+  override def createProject(repoId: String, projectName: String): Future[Project] = ???
 }
 
 class MockGithubService extends GithubService {
+  override def createColumn(projectId: String, column: Column): Future[Unit] =
+    Future.failed(new Exception("Stub!"))
 
+  override def createProject(repoId: String, projectName: String): Future[Project] =
+    Future.failed(new Exception("Stub!"))
 }
