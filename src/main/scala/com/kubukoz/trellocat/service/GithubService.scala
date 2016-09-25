aa2991d2abdf298565e4e50927b87f5751744e56
Jakub Kozłowski <kubukoz@gmail.com>
2016-09-25 21:12:02 +0200

    Move service mocks to the test source root

--- src/main/scala/com/kubukoz/trellocat/service/GithubService.scala
+++ src/main/scala/com/kubukoz/trellocat/service/GithubService.scala
@@ -89,15 +89,3 @@ class RealGithubService(ap: AuthParams)(implicit api: ApiClient, mat: Materializ
   protected def cardsUri(user: User, repoName: String, project: Project, column: Column): String =
     s"${columnsUri(user, repoName, project)}/${column.number}/cards"
 }
-
-//noinspection NotImplementedCode
-//todo move to test/
-class MockGithubService extends GithubService {
-  override def createProject(user: User, repoName: String, projectStub: ProjectStub)(implicit ec: ExecutionContext): Future[Project] = ???
-
-  override def createCard(user: User, project: Project, repoName: String, column: Column, card: Card)(implicit ec: ExecutionContext): Future[Card] = ???
-
-  override def createColumn(user: User, project: Project, repoName: String, columnStub: ColumnStub)(implicit ec: ExecutionContext): Future[Column] = ???
-
-  override def getUser()(implicit ec: ExecutionContext): Future[User] = ???
-}
--- src/main/scala/com/kubukoz/trellocat/service/TrelloService.scala
+++ src/main/scala/com/kubukoz/trellocat/service/TrelloService.scala
@@ -5,9 +5,7 @@ import akka.http.scaladsl.model.{HttpRequest, Uri}
 import akka.stream.Materializer
 import com.kubukoz.trellocat.api.ApiClient
 import com.kubukoz.trellocat.api.ApiClient.AuthenticatedUri
-import com.kubukoz.trellocat.domain.Trello.{Board, Column}
 import com.kubukoz.trellocat.domain.{AuthParams, JsonSupport, Trello}
-import com.kubukoz.trellocat.service.RealTrelloService._
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -28,6 +26,9 @@ trait TrelloService {
 class RealTrelloService(ap: AuthParams)(implicit api: ApiClient, mat: Materializer) extends TrelloService with JsonSupport {
   implicit val app = ap
 
+  val baseUrl = "https://api.trello.com/1"
+  val boardsUrl = s"$baseUrl/members/me/boards"
+
   override def allBoards(implicit ec: ExecutionContext): Future[List[Trello.Board]] =
     api[List[Trello.Board]](HttpRequest(uri = Uri(boardsUrl).withAuthQuery(Query.Empty)))
 
@@ -36,20 +37,4 @@ class RealTrelloService(ap: AuthParams)(implicit api: ApiClient, mat: Materializ
 
   override def boardById(boardId: String)(implicit ec: ExecutionContext): Future[Trello.Board] =
     api[Trello.Board](HttpRequest(uri = Uri(s"$baseUrl/boards/$boardId").withAuthQuery(Query.Empty)))
-}
-
-object RealTrelloService {
-  val baseUrl = "https://api.trello.com/1"
-  val boardsUrl = s"$baseUrl/members/me/boards"
-}
-
-trait MockTrelloService extends TrelloService {
-  override def allBoards(implicit ec: ExecutionContext): Future[List[Board]] =
-    Future.failed(new Exception("Stub!"))
-
-  override def columnsOnBoard(boardId: String)(implicit ec: ExecutionContext): Future[List[Column]] =
-    Future.failed(new Exception("Stub!"))
-
-  override def boardById(boardId: String)(implicit ec: ExecutionContext): Future[Board] =
-    Future.failed(new Exception("Stub!"))
 }
